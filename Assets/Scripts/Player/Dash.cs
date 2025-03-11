979beb6e2df6b40210a40a1b46125a31c733788a
Sirix <sirixb@gmail.com>
2025-03-11 16:19:17 -0500

    Feat: Dash Power Details
    Add particle effects, animation to Dash Power

--- Assets/Scripts/Player/Dash.cs
+++ Assets/Scripts/Player/Dash.cs
@@ -1,53 +1,49 @@
-﻿
-    using System;
-    using System.Collections;
-    using UnityEngine;
+﻿using System;
+using System.Collections;
+using UnityEngine;
 
-    public class Dash: MonoBehaviour
+public class Dash : MonoBehaviour
+{
+    [SerializeField] private PlayerController playerController;
+    [SerializeField] private TrailRenderer trailRenderer;
+    [SerializeField] private float dashSpeed = 4f;
+    [SerializeField] private float dashTime = .2f;
+    [SerializeField] private float dashCoolDown = .25f;
+
+    private float _startingMoveSpeed;
+    private bool _isDashing = false;
+
+    private void Awake()
     {
-        [SerializeField] private PlayerController playerController;
-        [SerializeField] private float dashSpeed = 4f;
-        [SerializeField] private TrailRenderer trailRenderer;
-        [SerializeField] private float dashTime = .2f;
-        [SerializeField] private float dashCoolDown = .25f;
-        
-        private float _startingMoveSpeed;
-        private bool _isDashing = false;
-
-        private void Awake()
-        {
-            playerController = GetComponent<PlayerController>();
-        }
-
-        private void Start()
-        {
-            _startingMoveSpeed = playerController.Speed;
-        }
-
-        private void Update()
-        {
-            if(Input.GetKeyDown(KeyCode.Space))
-                DoDash();
-        }
-
-        private void DoDash()
-        {
-            if (!_isDashing /*&& Stamina.Instance.CurrentStamina > 0*/)
-            {
-                // Stamina.Instance.UseStamina();
-                _isDashing = true;
-                playerController.Speed *= dashSpeed;
-                trailRenderer.emitting = true;
-                StartCoroutine(EndDashRoutine());
-                // Audio();
-            }
-        }
-        private IEnumerator EndDashRoutine()
-        {
-            yield return new WaitForSeconds(dashTime);
-            playerController.Speed = _startingMoveSpeed;
-            trailRenderer.emitting = false;
-            yield return new WaitForSeconds(dashCoolDown);
-            _isDashing = false;
-        }
+        playerController = GetComponent<PlayerController>();
     }
+
+    private void Start()
+    {
+        _startingMoveSpeed = playerController.Speed;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            DoDash();
+    }
+
+    private void DoDash()
+    {
+        if (_isDashing) return;
+        _isDashing = true;
+        playerController.Speed *= dashSpeed;
+        trailRenderer.emitting = true;
+        StartCoroutine(EndDashRoutine());
+    }
+
+    private IEnumerator EndDashRoutine()
+    {
+        yield return new WaitForSeconds(dashTime);
+        playerController.Speed = _startingMoveSpeed;
+        trailRenderer.emitting = false;
+        yield return new WaitForSeconds(dashCoolDown);
+        _isDashing = false;
+    }
+}
